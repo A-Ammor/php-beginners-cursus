63efa52c9290df7a1f46203b24fef522ba29c524
anwar <2003500@talnet.nl>
2017-05-27 19:15:09 +0200

    initial setup

--- main.php
+++ main.php
@@ -1,10 +1,4 @@
-<center>
 <?php
-	$kerstboom = 10;
-	for($i = 1; $i <= $kerstboom; $i++) {
-		while($j = 0; $j < $i; $j++) {
-			echo "*";
-		}
-		echo "<br>";
-	}
-
+	$a = "hello";
+	$b = "world!!!";
+    echo $a . " " . $b;
